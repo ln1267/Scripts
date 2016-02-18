4a11fe6faf85c541acaec7564068b0b0da1aa222
Ning <ln1267@gmail.com>
2016-02-18 22:18:38 +0800

    add comments for Parallel

--- Parallel.R
+++ Parallel.R
@@ -6,47 +6,47 @@ f_trend<-function(n){
   
   require(rkt)  # load required packages
   a<-rkt(lin_ann$YEAR, lin_ann[[n]])
-  return(c(n-1,a$sl,a$B)) # return targert values
+  return(c(n-1,a$sl,a$B)) # return targert values in a vector
 }
 
 f_NDVI<-function(n){
 new<-new.env()
 new<-apply(NDVI[,,n:n+1],c(1,2),max)
-return(new)
-#NDVI_new[,,(n+1)/2]<-apply(NDVI[,,n:n+1],c(1,2),max)
+return(new)		# return targert values in a matrix
+
 }
 
-
-##---- doParallel way
+#---- doParallel way
 
 library(doParallel)
-library(pryr)
-print("Size of NDVI")
-print(mem_used())
-cl<-makeCluster(detectCores()-1)  # set up parallel
+library(pryr) # for object size
 
-clusterEvalQ(cl, library(rms)) # load required packages
+cl<-makeCluster(detectCores()-1)  # set up parallel 
+
+clusterEvalQ(cl, library(rms)) # load required packages "rms"
 clusterExport(cl,c("NDVI","NDVI_flag","NDVI_new"))    # share default data for all threads
 
 tr<-parLapply(cl,seq(1,312,2),f_NDVI) # using parLapply to simulate data in parallel way
 
-# if return is a matrix, transfer list to array
-a<-array(unlist(tr), dim = c(nrow(tr[[1]]), ncol(tr[[1]]), length(tr)))
+## if return is a matrix, transfer list to array
+ts<-array(unlist(tr), dim = c(nrow(tr[[1]]), ncol(tr[[1]]), length(tr)))
+
+## combin general returned data
+ts<-do.call(rbind,tr) 
 
 stopCluster(cl)
 
-tr<-lapply(c(2:108501),f_trend)		# using lapply to simulate data in general way
+## using lapply to simulate data in nonparallel way
+tr<-lapply(c(2:108501),f_trend)		
 
-ts<-do.call(rbind,tr) # combin returned data
 
-##--- foreach way
-
-library(foreach)
+#--- foreach way
 library(doParallel)
+
 cl<-makeCluster(detectCores()-1)  # set up parallel
 registerDoParallel(cl)
 
-#start time
+##start time
 strt<-Sys.time()
 
 foreach(n = seq(1,312,2), .combine = rbind) %dopar% {
