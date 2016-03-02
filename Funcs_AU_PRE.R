05a8a14cf6e1a6a1538a7de4315c9d94051c89da
ning <ln1267@gmail.com>
2016-03-02 14:29:22 +0800

    add season and annual scale selection for f_dp function

--- Funcs_AU_PRE.R
+++ Funcs_AU_PRE.R
@@ -3,6 +3,12 @@
 # This file includs all functions used for studying the relationship between VEG and PRE in AU
 
 
+###---------list of functions
+#	f_parallel(data=null, fun=null, type="parallel")		for setting up parallel methods
+#	f_m2y(data, fun="mean")			for transfering monthly frame data to annual data by using fun="sum" or "mean"	
+#	f_summary()					for outputing summary infos of all "data frame objects" in memory
+#	f_dp(data,seasonal=TRUE,year_start,year_end)	for seasonal or annual changepoint and MK trend analysis
+
 ##################################################
 
 ## setup parallel for "parallel" or "doParallel" or "foreach" or "snow"
@@ -109,7 +115,7 @@ f_m2y<-function(data, fun="mean"){
   
 }
 
-## summary funtion which can output summary information for all objects 
+## summary funtion which can output summary information for all data frame objects in memory
 f_summary<-function(){
 	print("print info for all objects")
 	a<-ls(envir=.GlobalEnv)
@@ -124,47 +130,88 @@ f_summary<-function(){
 
 }  
 
+####################################################################
 ## changepoint detection using "bfast" package and MK test using "trend" package
-f_dp<-function(data,year_start,year_end){
+## in seasonal (default) and annual scale 
+## changepoint detection using "bfast" package 
+## http://www.sciencedirect.com/science/article/pii/S003442570900265X
+######################################################################
+
+f_dp<-function(data,seasonal=TRUE,year_start,year_end){
 	require(bfast)
 	require(trend)
+	require(pryr)
 	#.start<-(n-1)*((year_end-year_start+1)*12)+1
 	#.end<-n*((year_end-year_start+1)*12)
 	#print(n)
 	print(mem_used())
-	if(1){
-	if (!any(is.na(data))){
-	.linshi<-ts(data,frequency = 12,start = c(year_start,1))
 	
-	## changepoint detection using "bfast" package 
-	## http://www.sciencedirect.com/science/article/pii/S003442570900265X
-	
-	rdist<-12/length(.linshi)
-	fit <- bfast(.linshi,h=rdist, season="harmonic", max.iter=1,breaks=2)
-	.out<-fit$output[[1]]
-	if ( is.list(.out$bp.Vt)){.trend_change<-.out$bp.Vt$breakpoints}else{.trend_change<-NA}
-	if ( is.list(.out$ci.Wt)){.season_change<-.out$ci.Wt[[1]][2]}else{.season_change<-NA}
-	
-	## MK trend detection using "trend" package 
-	
-	.outmk<-smk.test(.linshi)
-	.outslope<-sea.sens.slope(.linshi)
+	if(seasonal){
+		# seasonal changepoint and trend detection
+		print("seasonal scale process")
+		# IF all data are NA or equal, there would be no trend
+		if (!(any(is.na(data)) | isTRUE(all.equal(data, rep(data[1], length(data)))))){
+		.linshi<-ts(data,frequency = 12,start = c(year_start,1))
+		
+		rdist<-12/length(.linshi)
+		# ratio of distance between breaks (time steps) and length of the time series
+		
+		fit <- bfast(.linshi,h=rdist, season="harmonic", max.iter=1,breaks=2)
+		.out<-fit$output[[1]]
+		if ( is.list(.out$bp.Vt)){.trend_change<-.out$bp.Vt$breakpoints}else{.trend_change<-NA}
+		if ( is.list(.out$ci.Wt)){.season_change<-.out$ci.Wt[[1]][2]}else{.season_change<-NA}
+		
+		## MK trend detection using "trend" package 
+		
+		.outmk<-smk.test(.linshi)
+		.outslope<-sea.sens.slope(.linshi)
 
+		}else{
+		
+		##---change point detect result
+		
+		.trend_change<-NA
+		.season_change<-NA
+		
+		## MK test result
+		
+		.outmk<-data.frame(tautot=NA,pvalue=NA)
+		.outslope<-data.frame(b.sen=NA)
+		
+		}
+		return(list(CP_trend=.trend_change,CP_season=.season_change,TAU=.outmk$tautot,PMK=.outmk$pvalue,SLOPE=.outslope$b.sen))
 	}else{
-	
-	##---change point detect result
-	
-	.trend_change<-NA
-	.season_change<-NA
-	
-	## MK test result
-	
-	.outmk<-data.frame(tautot=NA,pvalue=NA)
-	.outslope<-data.frame(b.sen=NA)
-	
+		# annual changepoint and trend detection
+		print("annual scale process")
+		# IF all data are NA or equal, there would be no trend
+		if (!(any(is.na(data)) | isTRUE(all.equal(data, rep(data[1], length(data)))))){
+		.linshi<-ts(data,frequency = 1, start = year_start)
+		
+		rdist<-3/length(.linshi)
+		#print(.linshi)
+		fit <- bfast(.linshi,h=rdist,season = "none", max.iter=1,breaks=2)
+		
+		.out<-fit$output[[1]]
+		
+		if ( is.list(.out$bp.Vt)){.trend_change<-.out$bp.Vt$breakpoints}else{.trend_change<-NA}
+				
+		## MK trend detection using "trend" package 
+		
+		.outmk<-mk.test(.linshi)
+		.outslope<-sens.slope(.linshi)
+
+		}else{
+		
+		##---change point detect result
+		
+		.trend_change<-NA
+				
+		## MK test result
+		
+		.outmk<-data.frame(tautot=NA,pvalue=NA)
+		.outslope<-data.frame(b.sen=NA)
+		
+		}
+		return(list(CP_trend=.trend_change,TAU=.outmk$tautot,PMK=.outmk$pvalue,SLOPE=.outslope$b.sen))
 	}
-	}
-	#return(list(n,.outmk$tautot,.outmk$pvalue,.outslope$b.sen))
-	#return(.sublinshi)ID=n,
-	return(list(CP_trend=.trend_change,CP_season=.season_change,TAU=.outmk$tautot,PMK=.outmk$pvalue,SLOPE=.outslope$b.sen))
 }
