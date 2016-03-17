6aafd144aa51678831e04a1690447b49ed4be718
ning <ln1267@gmail.com>
2016-03-17 09:28:59 +0800

    WaSSI result read and plot, and basin reading

--- WaSSI_OUT_AU.R
+++ WaSSI_OUT_AU.R
@@ -130,136 +130,42 @@ args <- commandArgs(TRUE)
 	#f_plot(RESULT[[outs[6]]],info=info,annual=T,monthly=T)
 
 	
+
+# --------start to validate and analyze AU result
 	
-if (0){
-load("RESULT_MJ_LCMerge.RData")
-load("Carbon_ann_MJ.RData")
-load("Carbon_ann_MJ_LCmerge.RData")
-Carbon_ann_LC_merge<-Carbon_ann
-
-
-print("Finish reading ENVI data")
-parameters<-read.delim(args[3],sep = " ",header = TRUE)
-YEAR_START<-args[4]
-print("the first year for WaSSI input is:")
-print(YEAR_START)
-YEAR_END<-args[5]
-print("the last year for WaSSI input is:")
-print(YEAR_END)
-print(parameters)
-#rm(list=ls())
-
-# read rows and cols of the study area
-nrows<-nrow(da)
-ncols<-ncol(da)
-print("nrows=")
-print(nrows)
-print("ncols=")
-print(ncols)
-# set default parameters for data processing
-## set the start and end year of input data for climate, LAI and land cover data
-S_y<-parameters[1,1]
-E_y<-parameters[2,1]
-S_y_LAI<-parameters[3,1]
-E_y_LAI<-parameters[4,1]
-S_y_LC<-parameters[5,1]
-E_y_LC<-parameters[6,1]
-
-## set the start line for each parameters of the ENVI data
-
-Line_DEM<-parameters[7,1]
-Line_S_PRE<-parameters[8,1]
-Line_E_PRE<-parameters[9,1]
-Line_S_TEMP<-parameters[10,1]
-Line_E_TEMP<-parameters[11,1]
-Line_S_LAI<-parameters[12,1]
-Line_E_LAI<-parameters[13,1]
-Line_S_LC<-parameters[14,1]
-Line_E_LC<-parameters[15,1]
-Line_S_SOIL<- parameters[16,1]
-
-## the coordation of the left-up pixel and the size of pixel  
-S_lat<- parameters[17,1]
-cell_size<-parameters[18,1]
-S_long<- parameters[19,1]
-#----------
-
-#---- read each variables
-print("read variables from ENVI data")
-Pre<-as.vector(da[,,Line_S_PRE:Line_E_PRE])
-Temp<-as.vector(da[,,Line_S_TEMP:Line_E_TEMP])
-LAI<-as.vector(da[,,Line_S_LAI:Line_E_LAI])
-LAI[LAI>=130]<-0 
-LC<-as.vector(da[,,Line_S_LC:Line_E_LC])
-S_soil<-Line_S_SOIL
-VEG<-as.vector(da[,,Line_S_LC-2])
-ALT<-as.vector(da[,,Line_DEM])
-##--- whether data needs to be transfered by dividing 10
-if  (parameters[20,1]) { 
-	Pre<-Pre/10.0
-	Temp<-Temp/10.0}
-if  (parameters[21,1]) { LAI<-LAI/10.0}
-if  (parameters[22,1]) { LC<-LC/10.0}
-if  (parameters[24,1]) { ALT<-ALT/10.0}
-
-#rm(da)
-#gc()
-
-#process climate data------------------------------
-
-Year_C<-rep(c(S_y:E_y), each=nrows*ncols*12)
-ID_C<-rep(c(1:(nrows*ncols)),12*(E_y-S_y+1))
-Month_C<-rep(rep(c(1:12), each=nrows*ncols),E_y-S_y+1)
-data_climate<-data.frame(ID=ID_C,YEAR=Year_C,Month=Month_C,Pre=Pre,Temp=Temp)
-system.time(
-data_climate<-arrange(data_climate,ID,YEAR,Month))
-# system.time(
-# with(data_climate,data_climate<<-data_climate[order(ID,YEAR,Month),]))
-print(summary(data_climate))
-data_climate<-subset(data_climate,YEAR>=YEAR_START & YEAR<=YEAR_END)
-write.table(data_climate,"INPUTS/CLIMATE.TXT",sep = ',',row.names = FALSE,col.names=FALSE)
-print("Finish climate data")
-rm(Year_C,ID_C,Month_C,Pre,Temp)
-#gc()
-#------------------------------
-
-Year_LAI<-rep(c(S_y_LAI:E_y_LAI), each=nrows*ncols*12)
-ID_LAI<-rep(c(1:(nrows*ncols)),12*(E_y_LAI-S_y_LAI+1))
-Month_LAI<-rep(rep(c(1:12), each=nrows*ncols),E_y_LAI-S_y_LAI+1)
-data_LAI<-data.frame(ID=ID_LAI,YEAR=Year_LAI,Month=Month_LAI,LAI=LAI)
-data_LAI<-arrange(data_LAI,ID,YEAR,Month)
-print(summary(data_LAI))
-data_LAI<-subset(data_LAI,YEAR>=YEAR_START & YEAR<=YEAR_END)
-write.table(data_LAI,"INPUTS/LANDLAI.TXT",sep = ',',row.names = FALSE,col.names=FALSE)
-print("Finish LAI data")
-rm(Year_LAI,ID_LAI,Month_LAI,LAI)
-#gc()
-#------------------------------
-
-SOIL<-data.frame(ID=c(1:(nrows*ncols)),UZTWM=as.vector(da[,,S_soil]),UZFWM=as.vector(da[,,S_soil+1]),UZK=as.vector(da[,,S_soil+2]),ZPERC=as.vector(da[,,S_soil+3]),REXP=as.vector(da[,,S_soil+4]),LZTWM=as.vector(da[,,S_soil+5]),LZFSM=as.vector(da[,,S_soil+6]),LZFPM=as.vector(da[,,S_soil+7]),LZSK=as.vector(da[,,S_soil+8]),LZPK=as.vector(da[,,S_soil+9]),PFREE=as.vector(da[,,S_soil+10]))
-print(summary(SOIL))
-for ( i in 2:12){ SOIL[[i]][is.na(SOIL[[i]])]<- -999}
-if  (parameters[23,1]) { SOIL<-SOIL/10;SOIL$ID<-c(1:(nrows*ncols))}
-write.table(SOIL,"INPUTS/SOILINFO.TXT",sep = ',',row.names = FALSE,col.names=FALSE)
-print("Finish SOILinfo data")
-#------------------------------
-
-LAT<-rep(seq(S_lat, by=-cell_size, length.out = nrows),ncols)
-LONG<-rep(seq(S_long, by=cell_size, length.out = ncols),each=nrows)
-data_cell<-data.frame(ID=c(1:(nrows*ncols)),LAT=LAT,LONG=LONG,VEG=VEG,ALT=ALT)
-print(summary(data_cell))
-write.table(data_cell,"INPUTS/CELLINFO.TXT",sep = ',',row.names = FALSE,col.names=FALSE)
-print("Finish cell info data")
-#------------------------------
-
-Year_LC<-rep(c(S_y_LC:E_y_LC), each=nrows*ncols)
-ID_LC<-rep(c(1:(nrows*ncols)),(E_y_LC-S_y_LC+1))
-data_LC<-data.frame(ID=ID_LC,YEAR=Year_LC,VEG=LC)
-data_LC<-arrange(data_LC,ID,YEAR)
-print(summary(data_LC))
-data_LC<-subset(data_LC,YEAR>=YEAR_START & YEAR<=YEAR_END)
-write.table(data_LC,"INPUTS/VEGINFO.TXT",sep = ',',row.names = FALSE,col.names=FALSE)
-print("Finish LUCC data")
-rm(Year_LC,ID_LC,LC)
-print("finsh data process")
-}
+	## load WA Basin boundary and infos and save it to Basin_WA.RData"
+	## this data is from WIN database
+	
+		if (file.exists("Basin_WA.RData")){
+			### if this file already exist
+			print('loading Basin data from Basin_WA.RData')
+			load("Basin_WA.RData")
+			load("Station_info.RData")
+		}else{
+			require(caTools)
+			Basin_WA<-read.ENVI("subcatchments_WA_5km")
+			Basin_WA<-data.frame(ID=c(1:564400),BASIN=as.vector(Basin_WA))
+			Basin_WA[Basin_WA==0]<-NA
+			Station_info<-read.csv("WIN_subcatchments_info.txt",header=T)
+			save(Basin_WA,file="Basin_WA.RData")
+			save(Station_info,file="Station_info.RData")
+		}
+	
+	## load AU Basin boundary and infos and save it to Basin_AU.RData"
+	## this data is from water storage database	
+		
+		if (file.exists("Basin_AU.RData")){
+			### if this file already exist
+			print('loading Basin data from Basin_AU.RData')
+			load("Basin_AU.RData")
+			load("Basin_AU_info.RData")
+		}else{
+			require(caTools)
+			Basin_AU<-read.ENVI("swma_Basins_5km")
+			Basin_AU<-data.frame(ID=c(1:564400),BASIN=as.vector(Basin_AU))
+			Basin_AU[Basin_AU==0]<-NA
+			Station_info<-read.csv("Basin_AU_info.txt",header=T)
+			save(Basin_AU,file="Basin_AU.RData")
+			save(Station_info,file="Basin_AU_info.RData")
+		}
+		
